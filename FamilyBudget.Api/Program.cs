7b58cd9cd24e0b62da4851f2772da81f136b2659
Sasha Kot <kotlikecat@gmail.com>
2022-12-15 17:43:16 +0100

    Add EF & Migrator to the DI

--- FamilyBudget.Api/Program.cs
+++ FamilyBudget.Api/Program.cs
@@ -1,3 +1,7 @@
+using FamilyBudget.Common.Models.Configuration;
+using FamilyBudget.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Dependency Injection
+var configuration = builder.Configuration;
+var connectionStrings = configuration.GetSection("ConnectionStrings").Get<ConnectionStrings>();
+
+#if DEBUG
+var connectionString = connectionStrings!.Local;
+#else
+var connectionString = connectionStrings!.Container;
+#endif
+
+// Entity Framework
+builder.Services.AddDbContext<DatabaseContext>(opt => opt.UseSqlServer(connectionString));
+
+// Migrator
+var migrator = new Migrator(connectionString);
+migrator.UpdateDatabase();
 
 var app = builder.Build();
 
