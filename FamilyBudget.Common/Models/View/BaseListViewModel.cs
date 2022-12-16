2d4c9dea9c7a8d34613f406d6db1b57a77abca47
Sasha Kot <kotlikecat@gmail.com>
2022-12-16 11:51:03 +0100

    Add empty constructor to BaseListViewModel

--- FamilyBudget.Common/Models/View/BaseListViewModel.cs
+++ FamilyBudget.Common/Models/View/BaseListViewModel.cs
@@ -2,12 +2,16 @@ namespace FamilyBudget.Common.Models.View;
 
 public class BaseListViewModel<T> where T:class
 {
+    public BaseListViewModel()
+    {
+        
+    }
     public BaseListViewModel(List<T> data, long length)
     {
         Data = data;
         Length = length;
     }
 
-    public List<T> Data { get; set; }
+    public List<T> Data { get; set; } = null!;
     public long Length { get; set; }
 }
