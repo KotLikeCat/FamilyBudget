f56b53f045e148248494e0edfe9d58116dbecd8b
Sasha Kot <kotlikecat@gmail.com>
2022-12-17 18:38:59 +0100

    Add Required Attributes To BudgetDetailInputModel

--- FamilyBudget.Common/Models/Input/BudgetDetailInputModel.cs
+++ FamilyBudget.Common/Models/Input/BudgetDetailInputModel.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FamilyBudget.Common.Models.Input;
 #pragma warning disable CS8618
 
 public class BudgetDetailInputModel
 {
+    [Required]
     public double Amount { get; set; }
+    
+    [Required]
     public Guid CategoryId { get; set; }
     public string? Description { get; set; }
+    
+    [Required]
     public bool IsIncome { get; set; }
 }
